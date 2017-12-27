68aa04fda5570a3054333e4a852022f3fc2c8b3b
Tetsuya Mori <monry84@gmail.com>
2017-12-27 16:39:27 +0900

    :tada: Implement scripts
    * PlayableBehaviour の Trigger となる Observable
    * TimeControl の Trigger となる Observable

---
+++ Assets/Scripts/UniRx/UnityEngineBridge/Triggers/ObservablePlayableBehaviourTrigger.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine.Playables;
+
+namespace UniRx.Triggers {
+
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    [SuppressMessage("ReSharper", "UseNullPropagation")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class ObservablePlayableBehaviourTrigger : PlayableBehaviour {
+
+        public class PlayableInfo {
+
+            public PlayableBehaviour PlayableBehaviour { get; }
+
+            public Playable Playable { get; }
+
+            public FrameData FrameData { get; }
+
+            public PlayableInfo(PlayableBehaviour playableBehaviour, Playable playable, FrameData frameData) {
+                PlayableBehaviour = playableBehaviour;
+                Playable = playable;
+                FrameData = frameData;
+            }
+
+        }
+
+        private Subject<PlayableInfo> onGraphStart;
+
+        // Called when the owning graph starts playing
+        public override void OnGraphStart(Playable playable) {
+            if (this.onGraphStart != null) {
+                this.onGraphStart.OnNext(new PlayableInfo(this, playable, default(FrameData)));
+            }
+        }
+
+        public IObservable<PlayableInfo> OnGraphStartAsObservable() {
+            return this.onGraphStart ?? (this.onGraphStart = new Subject<PlayableInfo>());
+        }
+
+        private Subject<PlayableInfo> onGraphStop;
+
+        // Called when the owning graph stops playing
+        public override void OnGraphStop(Playable playable) {
+            if (this.onGraphStop != null) {
+                this.onGraphStop.OnNext(new PlayableInfo(this, playable, default(FrameData)));
+            }
+        }
+
+        public IObservable<PlayableInfo> OnGraphStopAsObservable() {
+            return this.onGraphStop ?? (this.onGraphStop = new Subject<PlayableInfo>());
+        }
+
+        private Subject<PlayableInfo> onBehaviourPlay;
+
+        // Called when the state of the playable is set to Play
+        public override void OnBehaviourPlay(Playable playable, FrameData info) {
+            if (this.onBehaviourPlay != null) {
+                this.onBehaviourPlay.OnNext(new PlayableInfo(this, playable, info));
+            }
+        }
+
+        public IObservable<PlayableInfo> OnBehaviourPlayAsObservable() {
+            return this.onBehaviourPlay ?? (this.onBehaviourPlay = new Subject<PlayableInfo>());
+        }
+
+        private Subject<PlayableInfo> onBehaviourPause;
+
+        // Called when the state of the playable is set to Paused
+        public override void OnBehaviourPause(Playable playable, FrameData info) {
+            if (this.onBehaviourPause != null) {
+                this.onBehaviourPause.OnNext(new PlayableInfo(this, playable, info));
+            }
+        }
+
+        public IObservable<PlayableInfo> OnBehaviourPauseAsObservable() {
+            return this.onBehaviourPause ?? (this.onBehaviourPause = new Subject<PlayableInfo>());
+        }
+
+        private Subject<PlayableInfo> prepareFrame;
+
+        // Called each frame while the state is set to Play
+        public override void PrepareFrame(Playable playable, FrameData info) {
+            if (this.prepareFrame != null) {
+                this.prepareFrame.OnNext(new PlayableInfo(this, playable, info));
+            }
+        }
+
+        public IObservable<PlayableInfo> PrepareFrameAsObservable() {
+            return this.prepareFrame ?? (this.prepareFrame = new Subject<PlayableInfo>());
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/UniRx/UnityEngineBridge/Triggers/ObservableTimeControlTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace UniRx.Triggers {
+
+    [SuppressMessage("ReSharper", "UseNullPropagation")]
+    public class ObservableTimeControlTrigger : MonoBehaviour, ITimeControl {
+
+        private Subject<double> setTime;
+
+        public void SetTime(double time) {
+            if (this.setTime != null) {
+                this.setTime.OnNext(time);
+            }
+        }
+
+        public IObservable<double> SetTimeAsObservable() {
+            return this.setTime ?? (this.setTime = new Subject<double>());
+        }
+
+        private Subject<Unit> onControlTimeStart;
+
+        public void OnControlTimeStart() {
+            if (this.onControlTimeStart != null) {
+                this.onControlTimeStart.OnNext(Unit.Default);
+            }
+        }
+
+        public IObservable<Unit> OnControlTimeStartAsObservable() {
+            return this.onControlTimeStart ?? (this.onControlTimeStart = new Subject<Unit>());
+        }
+
+        private Subject<Unit> onControlTimeStop;
+
+        public void OnControlTimeStop() {
+            if (this.onControlTimeStop != null) {
+                this.onControlTimeStop.OnNext(Unit.Default);
+            }
+        }
+
+        public IObservable<Unit> OnControlTimeStopAsObservable() {
+            return this.onControlTimeStop ?? (this.onControlTimeStop = new Subject<Unit>());
+        }
+
+    }
+
+}
